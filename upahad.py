b1ad0e469f43d086f1df9997a64a4c6013dff255
Pratyush Saha <pattaboissxp2010@gmail.com>
2026-02-20 12:25:19 +0530

    added the file hehe

---
+++ upahad.py
@@ -0,0 +1,28 @@
+#gift for 9th March 2026
+#Simple code to wish
+import sys
+print("Happy Birhday ! ✨")
+
+
+
+class spandana:
+    def __init__(self, date, wish, name):
+        self.date = int(date)
+        self.wish = wish
+        self.name = name
+
+
+pichai = spandana(9, " Some other day", "Spandana")
+
+def satya_nadella(lie):
+    print("Do you think you are dumb?")
+
+    bday = pichai.date
+    if "Thank you" or "thx" in lie:
+        print(f"Happy Bday {bday} March")
+
+    
+
+satya_nadella(input("ENTER SOMETHING! "))
+print(f"if you have any wish then expect, {pichai.wish}")
+
